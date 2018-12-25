1af876f1d10315154a83f4b036368f6beddb3f19
jianfengtheboy <jianfengtheboy@163.com>
2018-12-25 23:04:10 +0800

    renew in 12.25 23:05pm

---
+++ common/script/fetch.js
@@ -0,0 +1,49 @@
+let config = require('./config.js');
+let message = require('../../component/message/message.js')
+
+//获取电影列表
+function fetchFilms(url, start, count, cb, fail_cb) {
+  let that = this;
+  message.hide.call(that)
+  if (that.data.hasMore) {
+    wx.request({
+      url: url,
+      data: {
+        city: config.city,
+        start: start,
+        count: config.count
+      },
+      method: 'GET',
+      header: {
+        "Content-Type": "application/json,application/json"
+      },
+      success: function(res) {
+        if (res.data.subjects.length ===0) {
+          that.setData({
+            hasMore: false
+          })
+        } else {
+          that.setData({
+            films: that.data.films.concat(res.data.subjects),
+            start: that.data.start + res.data.subjects.length,
+            showLoading: false
+          })
+        }
+        wx.stopPullDownRefresh()
+        typeof cb == 'function' && cb(res.data)
+      },
+      fail: function() {
+        that.setData({
+          showLoading: false
+        })
+        message.show.call(that, {
+          content: '网络开小差了',
+          icon: 'offline',
+          duration: 3000
+        })
+        wx.stopPullDownRefresh()
+        typeof fail_cb == 'function' && fail_cb()
+      }
+    })
+  }
+}
