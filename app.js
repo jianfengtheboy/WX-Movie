85ceee35bdb3f0b125ec09cfde26797d5aefc36f
jianfengtheboy <jianfengtheboy@163.com>
2019-01-10 23:50:57 +0800

    renew in 01.10 23:50pm

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 //app.js
 let config = require('common/script/config');
 App({
-  onLaunch: function () {
-    
-  },
   globalData: {
     userInfo: null
+  },
+  onLaunch: function () {
+    //获取用户信息
+    this.getUserInfo()
+    //初始化缓存
+    this.initStorage()
+  },
+  getUserInfo: function(cb) {
+    let that = this
+    wx.login({
+      success: function() {
+        wx.getUserInfo({
+          success: function (res) {
+            that.globalData.userInfo = res.userInfo
+            typeof cb == "function" && cb(that.globalData.userInfo)
+          }
+        })
+      }
+    })
+  },
+  getCity: function(cb) {
+
+  },
+  initStorage: function() {
+    
   }
 })
