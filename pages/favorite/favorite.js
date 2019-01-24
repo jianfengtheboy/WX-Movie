1fcbfd334e4000891d94370ec5480603444efc5b
jianfengtheboy <jianfengtheboy@163.com>
2019-01-24 23:26:05 +0800

    renew in 01.24 23:25pm

--- pages/favorite/favorite.js
+++ pages/favorite/favorite.js
@@ -1,66 +1,58 @@
 // pages/favorite/favorite.js
+let filmNullTip = {
+  tipText: "亲，找不到电影的收藏",
+  actionText: "去逛逛",
+  routeUrl: "../popular/popular"
+}
+let personNullTip = {
+  tipText: "亲，找不到人物的收藏",
+  actionText: "去逛逛",
+  routeUrl: "../popular/popular"
+}
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    film_favorite: [],
+    person_favorite: [],
+    show: "film_favorite",
+    nullTip: filmNullTip
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let that = this
+    wx.getStorage({
+      key: "film_favorite",
+      success: function(res){
+        that.setData({
+          film_favorite: res.data
+        })
+      }
+    })
+    wx.getStorage({
+      key: "person_favorite",
+      success: function(res){
+        that.setData({
+          person_favorite: res.data
+        })
+      }
+    })
+    wx.stopPullDownRefresh()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  viewFilmDetail: function(e) {
+		var data = e.currentTarget.dataset
+		wx.redirectTo({
+			url: "../filmDetail/filmDetail?id=" + data.id
+		})
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  viewPersonDetail: function(e) {
+		var data = e.currentTarget.dataset
+		wx.redirectTo({
+			url: "../personDetail/personDetail?id=" + data.id
+		})
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  changeViewType: function(e) {
+    var data = e.currentTarget.dataset
+    this.setData({
+      show: data.type,
+      nullTip: data.type == "film_favorite" ? filmNullTip : personNullTip
+    })
   }
 })
