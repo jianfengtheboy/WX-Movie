ecfdf1d1ad1036a0ea74af17a123b3d37a5530f9
jianfengtheboy <jianfengtheboy@163.com>
2019-01-25 23:53:52 +0800

    renew in 01.25 23:53pm

--- pages/editPersonInfo/editPersonInfo.js
+++ pages/editPersonInfo/editPersonInfo.js
@@ -1,66 +1,103 @@
 // pages/editPersonInfo/editPersonInfo.js
+let util = require("../../utils/util")
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: "",
+    nickName: "",
+    gender: 0,
+    genderArray: ["男", "女"],
+    genderIndex: 0,
+    age: 0,
+    birthday: "",
+    constellation: "",
+    constellationArray: ["白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座"],
+    constellationIndex: 0,
+    company: "",
+    school: "",
+    tel: "",
+    email: "",
+    intro: "",
+    birthdayEndDate: ""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let birthdayEndDate = util.getDate()
+    let that = this
+    wx.getStorage({
+      key: "person_info",
+      success: function(res){
+        let data = res.data
+        that.setData({
+          name: data.name,
+          nickName: data.nickName,
+          gender: data.gender,
+          age: data.age,
+          birthday: data.birthday,
+          constellation: data.constellation,
+          company: data.company,
+          school: data.school,
+          tel: data.tel,
+          email: data.email,
+          intro: data.intro,
+          birthdayEndDate: birthdayEndDate
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  savePersonInfo: function(e) {
+    let data = e.detail.value
+    wx.setStorage({
+      key: "person_info",
+      data: {
+        name: data.name,
+        nickName: data.nickName,
+        gender: data.gender,
+        age: data.age,
+        birthday: data.birthday,
+        constellation: data.constellation,
+        company: data.company,
+        school: data.school,
+        tel: data.tel,
+        email:data.email,
+        intro: data.intro
+      },
+      success: function(res){
+        wx.showToast({
+          title: "资料修改成功",
+          icon: "success",
+          duration: 2000
+        })
+        setTimeout(function(){
+          wx.navigateTo({
+            url: "../personInfo/personInfo"
+          })
+        },2000)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  changeGender: function(e) {
+    let genderIndex = e.detail.value
+    if (genderIndex != "null") {
+      this.setData({
+        genderIndex: genderIndex,
+        gender: this.data.genderArray[this.data.genderIndex]
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  changeBirthday: function(e) {
+    let birthday = e.detail.value
+    if (birthday != "null") {
+      this.setData(
+        {birthday: birthday}
+      )
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  changeConstellation: function(e) {
+    let constellationIndex = e.detail.value
+    if (constellationIndex != "null") {
+      this.setData({
+        constellationIndex: constellationIndex,
+        constellation: this.data.constellationArray[this.data.constellationIndex]
+      })
+    }
   }
 })
--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,66 +1,53 @@
 // pages/gallery/gallery.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    pictures: [],
+    nullTip: {
+      tipText: "亲，没有上传照片哦",
+      actionText: "上传",
+      fn: "uploadImg"
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    let that = this
+    wx.getStorage({
+      key: 'gallery',
+      success: function(res){
+        that.setData({
+          pictures: res.data
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  uploadImg: function() {
+    let that = this
+    wx.chooseImage({
+      count: 1,
+      success: function(res) {
+        let tempFilePath = res.tempFilePaths[0]
+        wx.saveFile({
+          tempFilePath: tempFilePath,
+          success: function(res) {
+            let savedFilePath = res.savedFilePath
+            that.setData({
+              pictures: that.data.pictures.concat(savedFilePath)
+            })
+            wx.setStorage({
+              key: "gallery",
+              data: that.data.pictures
+            })
+          }
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  previewImage: function(e) {
+    let data =  e.currentTarget.dataset
+    let index = data.index
+    let that = this
+    wx.previewImage({
+      current: that.data.pictures[index], // 当前显示图片的链接，不填则默认为 urls 的第一张
+      urls: that.data.pictures
+    })
   }
 })
